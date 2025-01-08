4476c4ed5a46ea5e8ccb3bcda7687c25833768b0
Hersh 🍫 <phersh05@gmail.com>
2025-01-08 16:28:56 -0600

    setup blank photos page

---
+++ src/app/photos/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function Photos() {
+  return (
+    <main className="min-h-screen bg-white p-8">
+      <Link href="/" className="text-[1.6rem] link-style">&larr; back</Link>
+    </main>
+  )
+}
