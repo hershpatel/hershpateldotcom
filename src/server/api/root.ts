095cf8077ceefdd481bf826de7f6e6457588bbbe
Hersh 🍫 <phersh05@gmail.com>
2025-01-13 19:33:15 -0500

    remove default trpc post router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { s3Router } from "~/server/api/routers/s3";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
@@ -8,7 +7,6 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   s3: s3Router,
 });
 
