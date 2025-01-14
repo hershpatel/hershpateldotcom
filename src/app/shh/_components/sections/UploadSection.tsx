9c7cc9982b2e327ca8e2c087a2b4787472d73a1a
Hersh 🍫 <phersh05@gmail.com>
2025-01-13 22:59:39 -0500

    dynamic sizing

--- src/app/shh/_components/sections/UploadSection.tsx
+++ src/app/shh/_components/sections/UploadSection.tsx
@@ -2,7 +2,7 @@
 
 export function UploadSection() {
   return (
-    <div className="text-[1.6rem]">
+    <div className="text-[1.2rem] sm:text-[1.4rem] md:text-[1.6rem]">
       <p>Butterfly dance through summer breeze</p>
     </div>
   );
